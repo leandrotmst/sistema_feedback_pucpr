8ff8301dba33606377131dfd816aca41d92382b9
Yumi Komatsu <yumikomatsunkc@gmail.com>
2026-04-08 13:16:52 -0300

    funcao logout e botoes voltar

--- js/gestor_valida_sessao.js
+++ js/gestor_valida_sessao.js
@@ -1,8 +1,18 @@
+console.log("JS carregado");
 async function gestor_valida_sessao(){
     const retorno = await fetch('../php/gestor_valida_sessao.php');
     const resposta = await retorno.json();
 
     if(resposta.status=='nok'){
+        window.location.href = '../index.html';
+    }
+}
+
+async function logout(){
+    const retorno = await fetch('../php/gestor_logout.php');
+    const resposta = await retorno.json();
+
+    if(resposta.status == 'ok'){
         window.location.href = '../index.html';
     }
 }
